4157d73b02ec937d4caaf9068547312bbf4c384f
Steven <Steven@Stevens-iMac.local>
2020-03-23 16:20:26 +0000

    added limits to card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,17 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit
+  CARD_LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
+    @limit = CARD_LIMIT
   end
 
   def top_up(money)
-    @balance += money
+    if @balance + money > @limit
+      raise 'exceeded limit of £90!'
+    else
+      @balance += money
+    end
   end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -14,4 +14,14 @@ describe Oystercard do
     card.top_up(5)
     expect(card::balance).to eq 5
   end
+  
+  describe 'top_up' do 
+  it "limits the customer to a max of £90" do 
+    expect{subject.top_up(91)}.to raise_error 
+  end
+  
+  it "checks to see the limit is £90" do 
+    expect(subject::limit).to eq 90
+  end
+end
 end

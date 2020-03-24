906477316fb30e1fd87c4ff0792ce42887e6b799
Harry Turnbull <hturnbull@live.co.uk>
2020-03-24 15:19:35 +0000

    refactored guard clauses for topup and touchin

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,18 @@
 class Oystercard
   attr_reader :balance, :limit
   CARD_LIMIT = 90
+  STARTING_BALANCE = 0
+  MINIMUM_TO_TRAVEL = 1
 
-  def initialize(balance = 0)
-    @balance = balance
+  def initialize
+    @balance = STARTING_BALANCE
     @limit = CARD_LIMIT
     @in_journey = false
   end
 
   def top_up(money)
-    if @balance + money > @limit
-      raise 'exceeded limit of £90!'
-    else
-      @balance += money
-    end
+    raise 'exceeded limit of £90!' if exceed_limit?(money)
+    @balance += money
   end
 
   def deduct(fair)
@@ -25,11 +24,21 @@ class Oystercard
   end
 
   def touch_in
-    raise 'Balance under minimum amount to touch in' unless balance >= 1
+    raise 'Balance under minimum amount to touch in' if under_minimum_balance?
     @in_journey = true
   end
 
   def touch_out
     @in_journey = false
+  end
+
+  private
+
+  def under_minimum_balance?
+    balance < MINIMUM_TO_TRAVEL
+  end
+
+  def exceed_limit?(money)
+    balance + money > limit
   end
 end
